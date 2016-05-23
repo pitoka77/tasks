3fd8b6c8790b3c7a8bcbb519d7dd7a22f41529e2
pitoka77 <wer-93@mail.ru>
2016-05-23 14:24:46 +0900

    Add files via upload

---
+++ 20160324170125_createtasks.rb
@@ -0,0 +1,9 @@
+class Createtasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.integer :list_id
+      t.integer :user_id
+    end
+  end
+end
